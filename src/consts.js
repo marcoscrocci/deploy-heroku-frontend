c1b03e5ede9b17866029a2b90bf4f6234bd64c66
marcoscrocci@gmail.com <marcoscrocci@gmail.com>
2019-11-10 22:13:34 -0300

    Ajustando URLs do backend

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export default {
-    API_URL: 'https://cod3r-my-money-app-backend.herokuapp.com/api',
-    OAPI_URL: 'https://cod3r-my-money-app-backend.herokuapp.com/oapi',
+    API_URL: 'https://marcos-my-money-app-backend.herokuapp.com/api',
+    OAPI_URL: 'https://marcos-my-money-app-backend.herokuapp.com/oapi',
 }
